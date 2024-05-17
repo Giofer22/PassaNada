9f4552fcfa610adbb792c0dad776294f71c8dcd0
Giovanni Fernandes <gfmsantos22@gmail.com>
2024-05-16 22:16:25 -0300

    Adicionar arquivos de projeto.

---
+++ PassaNada/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.lblEMail = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtSenha = New System.Windows.Forms.TextBox()
+        Me.lblSenha = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnLimpar = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblEMail
+        '
+        Me.lblEMail.AutoSize = True
+        Me.lblEMail.Font = New System.Drawing.Font("Fira Code SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEMail.Location = New System.Drawing.Point(192, 76)
+        Me.lblEMail.Name = "lblEMail"
+        Me.lblEMail.Size = New System.Drawing.Size(69, 20)
+        Me.lblEMail.TabIndex = 0
+        Me.lblEMail.Text = "Email:"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(196, 99)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(264, 20)
+        Me.txtEmail.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(11, 76)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(166, 151)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'txtSenha
+        '
+        Me.txtSenha.Location = New System.Drawing.Point(196, 159)
+        Me.txtSenha.Name = "txtSenha"
+        Me.txtSenha.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtSenha.Size = New System.Drawing.Size(264, 20)
+        Me.txtSenha.TabIndex = 4
+        '
+        'lblSenha
+        '
+        Me.lblSenha.AutoSize = True
+        Me.lblSenha.Font = New System.Drawing.Font("Fira Code SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSenha.Location = New System.Drawing.Point(192, 136)
+        Me.lblSenha.Name = "lblSenha"
+        Me.lblSenha.Size = New System.Drawing.Size(69, 20)
+        Me.lblSenha.TabIndex = 3
+        Me.lblSenha.Text = "Senha:"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Font = New System.Drawing.Font("Fira Code SemiBold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLogin.Location = New System.Drawing.Point(385, 204)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 5
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnLimpar
+        '
+        Me.btnLimpar.Font = New System.Drawing.Font("Fira Code SemiBold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpar.Location = New System.Drawing.Point(196, 204)
+        Me.btnLimpar.Name = "btnLimpar"
+        Me.btnLimpar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpar.TabIndex = 6
+        Me.btnLimpar.Text = "Limpar"
+        Me.btnLimpar.UseVisualStyleBackColor = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(486, 313)
+        Me.Controls.Add(Me.btnLimpar)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtSenha)
+        Me.Controls.Add(Me.lblSenha)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.lblEMail)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblEMail As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtSenha As TextBox
+    Friend WithEvents lblSenha As Label
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnLimpar As Button
+End Class
